af9deadb0788b37a8dd4e214a5f818040407e970
santhoshBE2022 <be.santhoshkumar@gmail.com>
2022-02-01 15:55:54 +0530

    Pushing inqude Framework

---
+++ src/test/java/com/crm/comcast/contact/ContactTest.java
@@ -0,0 +1,33 @@
+package com.crm.comcast.contact;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class ContactTest 
+{
+	@Test(groups="smokeTest")
+	public void createContact() throws Throwable
+	{		
+		System.out.println("Executing createContact test");		
+	}
+	
+	@Test(groups="regressionTest")
+	public void modifyContact()
+	{
+				
+		System.out.println("Executing modifyContact test");
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
